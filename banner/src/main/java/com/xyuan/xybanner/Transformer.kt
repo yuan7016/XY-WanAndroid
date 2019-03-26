54c9165f920e3d36cae87e7c6a46840f57cc08fa
xy7016 <yuanzq1992@163.com>
2019-03-26 10:27:41 +0800

    banner for androidx

---
+++ banner/src/main/java/com/xyuan/xybanner/Transformer.kt
@@ -0,0 +1,18 @@
+package com.xyuan.xybanner
+
+
+import androidx.viewpager.widget.ViewPager.PageTransformer
+import com.xyuan.xybanner.transformer.*
+
+/**
+ * 轮播Transformer样式
+ */
+object Transformer {
+    //默认
+    var Default: Class<out PageTransformer> = DefaultTransformer::class.java
+    var Accordion: Class<out PageTransformer> = AccordionTransformer::class.java
+    var ForegroundToBackground: Class<out PageTransformer> = ForegroundToBackgroundTransformer::class.java
+    var CubeIn: Class<out PageTransformer> = CubeInTransformer::class.java
+    var Stack: Class<out PageTransformer> = StackTransformer::class.java
+    var Tablet: Class<out PageTransformer> = TabletTransformer::class.java
+}
